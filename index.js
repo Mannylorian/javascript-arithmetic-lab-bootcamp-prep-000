8cc818f8f1c8528cce4b2578ef34757290f5c9f5
Emmanuel Anderson <manu1526@gmail.com>
2018-05-01 15:03:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+let a = 518;
+let b = 516;
+ function add(a, b){
+  return a+b;
+}
+add(a, b);
+
+let a = 518;
+let b = 516;
+function subtract(a, b){
+  return a-b;
+}
+subtract(a, b);
+
+   let a = 518
+  let b = 516
+function add(a, b){
+  return a+b;
+}
+add(a, b);
+
    let a = 518
   let b = 516
 function add(a, b){
