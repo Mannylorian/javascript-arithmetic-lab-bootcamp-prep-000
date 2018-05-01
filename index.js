38a0e1065d74f8bb91bd20342473eb84a6043bb4
Emmanuel Anderson <manu1526@gmail.com>
2018-05-01 15:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ add(a, b);
 
 let a = 518;
 let b = 516;
-function subtract(a, b){
+ function subtract(a, b){
   return a-b;
 }
 subtract(a, b);
 
-   let a = 518
-  let b = 516
-function add(a, b){
-  return a+b;
+let a = 518;
+let b = 516;
+ function multiply(a, b){
+  return a*b;
 }
-add(a, b);
+multiply(a, b);
 
-   let a = 518
-  let b = 516
-function add(a, b){
-  return a+b;
+let a = 518;
+let b = 516;
+ function divide(a, b){
+  return a/b;
 }
-add(a, b);
+divide(a, b);
