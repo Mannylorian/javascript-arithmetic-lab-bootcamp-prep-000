909b8eb2181d2fd4884d1070c67b2e823d1a77ea
Emmanuel Anderson <manu1526@gmail.com>
2018-05-01 15:06:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-let a = 518;
-let b = 516;
+ let a = Math.floor(Math.random() * 1000)
+ let b = Math.floor(Math.random() * 1000)
  function add(a, b){
   return a+b;
 }
 add(a, b);
 
-let a = 518;
-let b = 516;
+ let a = Math.floor(Math.random() * 1000)
+ let b = Math.floor(Math.random() * 1000)
  function subtract(a, b){
   return a-b;
 }
 subtract(a, b);
 
-let a = 518;
-let b = 516;
+ let a = Math.floor(Math.random() * 1000)
+ let b = Math.floor(Math.random() * 1000)
  function multiply(a, b){
   return a*b;
 }
 multiply(a, b);
 
-let a = 518;
-let b = 516;
+ let a = Math.floor(Math.random() * 1000)
+ let b = Math.floor(Math.random() * 1000)
  function divide(a, b){
   return a/b;
 }
